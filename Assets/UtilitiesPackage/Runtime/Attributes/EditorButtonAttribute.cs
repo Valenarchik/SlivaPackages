40c1f5a62c3a155f0ac6a9e6c78016162d21c279
Pavel <pavel.mokr2005@gmail.com>
2024-05-28 14:15:05 +0500

    add custom utilities package

---
+++ Assets/UtilitiesPackage/Runtime/Attributes/EditorButtonAttribute.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace Utilities
+{
+    [System.AttributeUsage(System.AttributeTargets.Method)]
+    public class EditorButtonAttribute : PropertyAttribute
+    {
+    }
+}
