df4df180bcce1bbc0c24310996323804404207b6
Pavel <pavel.mokr2005@gmail.com>
2024-06-13 10:35:04 +0500

    Update IndexList

--- Assets/UtilitiesPackage/Runtime/DataStructures/IndexList.cs
+++ Assets/UtilitiesPackage/Runtime/DataStructures/IndexList.cs
@@ -1,53 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
-using UnityEngine;
 using System;
 
 namespace DataStructures
 {
     public class IndexList<T> : IReadOnlyIndexList<T>
     {
-        private int currentIndex = -1;
+        private int lastIndex;
         private readonly Dictionary<int, T> indexToObject;
         private readonly Dictionary<T, int> objectToIndex;
         private readonly HashSet<int> indexesSet;
 
         public IReadOnlyDictionary<T, int> ObjectToIndex => objectToIndex;
+        public IReadOnlyDictionary<int, T> IndexToObject => indexToObject;
 
-        public IndexList() 
+        public int Count => indexesSet.Count;
+        public IEnumerable<int> Keys => indexesSet;
+        public IEnumerable<T> Values => indexToObject.Values;
+
+        public IndexList()
         {
+            lastIndex = 0;
             indexToObject = new Dictionary<int, T>();
             objectToIndex = new Dictionary<T, int>();
-            indexesSet = new HashSet<int>();    
+            indexesSet = new HashSet<int>();
         }
 
-        public IndexList(IReadOnlyDictionary<int, T> dict) :this()
+        public IndexList(IReadOnlyDictionary<int, T> dict) : this()
         {
             var maxIndex = int.MinValue;
             foreach (var pair in dict)
             {
                 indexToObject[pair.Key] = pair.Value;
                 objectToIndex[pair.Value] = pair.Key;
-                maxIndex = Mathf.Max(maxIndex, pair.Key);
+                maxIndex = Math.Max(maxIndex, pair.Key);
             }
-            currentIndex = maxIndex;
+
+            lastIndex = maxIndex + 1;
         }
 
         public int Add(T item)
         {
-            currentIndex++;
-            while(indexesSet.Contains(currentIndex))
-                currentIndex++;
-            
-            var index = currentIndex;
+            var index = lastIndex;
             Add(index, item);
+            lastIndex++;
+            while (indexesSet.Contains(lastIndex))
+                lastIndex++;
 
             return index;
         }
 
         public void Add(int index, T item)
         {
-            if (indexesSet.Contains(index)) 
+            if (indexesSet.Contains(index))
                 throw new ArgumentException("Index is already taken!");
 
             indexToObject[index] = item;
@@ -60,7 +65,6 @@ namespace DataStructures
             var index = objectToIndex[item];
             indexToObject.Remove(index);
             objectToIndex.Remove(item);
-            indexesSet.Remove(index);
         }
 
         public void Remove(int index)
@@ -71,56 +75,30 @@ namespace DataStructures
 
         public void Clear()
         {
-            currentIndex = 0;
+            lastIndex = 0;
             indexToObject.Clear();
             objectToIndex.Clear();
-            indexesSet.Clear();
         }
 
-        public static IndexList<T> FromList(IEnumerable<T> list)
+        public IEnumerator<T> GetEnumerator()
         {
-            var indexList = new IndexList<T>();
-
-            foreach(var item in list)
-            {
-                indexList.Add(item);
-            }
-
-            return indexList;
-        }
-
-
-
-        #region IReadOnlyDictionary Realization
-        public bool ContainsKey(int key)
-        {
-            return indexToObject.ContainsKey(key);
-        }
-
-        public IEnumerator<KeyValuePair<int, T>> GetEnumerator()
-        {
-            return indexToObject.GetEnumerator();
-        }
-
-        public bool TryGetValue(int key, out T value)
-        {
-            return indexToObject.TryGetValue(key, out value);
+            return Values.GetEnumerator();
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            return ((IEnumerable)indexToObject).GetEnumerator();  
+            return GetEnumerator();
         }
-
-        public IEnumerable<int> Keys => indexToObject.Keys;
-        public IEnumerable<T> Values => indexToObject.Values;
-        public int Count => indexToObject.Count;
-        public T this[int key] => indexToObject[key];
-        #endregion
     }
 
-    public interface IReadOnlyIndexList<T> : IReadOnlyDictionary<int, T>
+    public interface IReadOnlyIndexList<T>: IEnumerable<T>
     {
+        public IReadOnlyDictionary<int, T> IndexToObject { get; }
         public IReadOnlyDictionary<T, int> ObjectToIndex { get; }
+
+        public int Count { get; }
+
+        public IEnumerable<int> Keys { get; }
+        public IEnumerable<T> Values { get; }
     }
 }
