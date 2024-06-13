797e154e0aa27dd30c06b888126b52a3eccee4b4
Pavel <pavel.mokr2005@gmail.com>
2024-06-13 10:50:49 +0500

    add rect transform extensions

---
+++ Assets/UtilitiesPackage/Runtime/Extensions/RectTransformExtensions.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+
+public static class RectTransformExtension
+{
+    public static void ReScaleImageIntoRectTransform(this RectTransform imageRect, RectTransform containerTransform)
+    {
+        var widthDifference = imageRect.rect.width - containerTransform.rect.width;
+        var heightDifference = imageRect.rect.height - containerTransform.rect.height;
+
+        if (widthDifference > 0 || heightDifference > 0)
+        {
+            if (heightDifference > widthDifference)
+            {
+                imageRect.sizeDelta = imageRect.rect.size * (containerTransform.rect.size.y / imageRect.rect.size.y);
+            }
+            else
+            {
+                imageRect.sizeDelta = imageRect.rect.size * (containerTransform.rect.size.x / imageRect.rect.size.x);
+            }
+        }
+    }
+}
